815973df2bf8989f076ab3f1db809a44833a2e75
OlegYankiwskyi <Oleg_Yankiwskyi@epam.com>
2018-04-18 14:21:29 +0300

    big and small text

--- TableView/DetailController.swift
+++ TableView/DetailController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DetailController: UIViewController {
 
+    @IBOutlet weak var viewForOpacity: UIView!
     @IBOutlet weak var buttonConstrain: NSLayoutConstraint!
     @IBOutlet weak var heightConstrain: NSLayoutConstraint!
     @IBOutlet weak var buttonShowText: UIButton!
@@ -18,12 +19,13 @@ class DetailController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+//        viewForOpacity.opacityGradient()
         textLabel.text = modelEntity?.getDescript() ?? "We don`t have description for this ATD"
         self.title = modelEntity?.getTitle() ?? "Default ATD"
     }
     
     @IBAction func onButtonShowText(_ sender: Any) {
-        print(123)
         if buttonShowText.currentTitle! == "More" {
             heightConstrain.priority = UILayoutPriority(rawValue: 250)
             buttonShowText.setTitle("Less", for: .normal)
@@ -39,5 +41,16 @@ class DetailController: UIViewController {
 //            self.view.updateConstraints()
 
         }
+    }
+}
+
+extension UIView {
+    func opacityGradient() {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.superview?.bounds ?? CGRect.null
+        gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor, UIColor.black.cgColor, UIColor.clear.cgColor]
+        gradient.locations = [0.0, 0.15, 0.25, 0.75, 0.85, 1.0]
+        self.superview?.layer.mask = gradient
+        self.backgroundColor = UIColor.clear
     }
 }
